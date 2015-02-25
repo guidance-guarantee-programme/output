ee83258b1b7bd7309856ab4147bc2de6aa6dbbb8
Alistair Duggin <alistair.duggin@btinternet.com>
2015-02-25 18:00:27 +0000

    Add form, input and output pages to style guide

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -4,4 +4,12 @@ class StyleguideController < ApplicationController
 
   def base
   end
+
+  def pages_input
+    render template: 'styleguide/pages/input'
+  end
+
+  def pages_output
+    render template: 'styleguide/pages/output'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   constraints format: 'html' do
     scope path: 'styleguide', controller: 'styleguide' do
+      scope path: 'pages' do
+        get 'input', action: 'pages_input'
+        get 'output', action: 'pages_output'
+      end
+
       get '(/:action)'
     end
   end
