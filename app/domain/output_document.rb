9e4bbfd249b8e6e41e89f331c446da0a060b1424
Steven Wilkin <stevenwilkin@gmail.com>
2015-02-27 16:31:23 +0000

    Use a domain object to generate record of guidance

---
+++ app/domain/output_document.rb
@@ -0,0 +1,22 @@
+class OutputDocument
+  attr_accessor :appointment_summary
+
+  def initialize(appointment_summary)
+    @appointment_summary = appointment_summary
+  end
+
+  def html
+    ERB.new(template).result(binding)
+  end
+
+  def pdf
+    WickedPdf.new.pdf_from_string(html)
+  end
+
+  private
+
+  def template
+    File.read(
+      File.join(Rails.root, 'app', 'templates', 'output_document.html.erb'))
+  end
+end
