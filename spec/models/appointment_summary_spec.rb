74c8c4a218c76f9ca5bbc0de6c0ac462f0d5722c
Andrew Garner <andrew@andrewgarner.com>
2015-03-03 11:02:44 +0000

    Collect value of pension pots

--- spec/models/appointment_summary_spec.rb
+++ spec/models/appointment_summary_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe AppointmentSummary, type: :model do
   subject { described_class.new }
 
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:value_of_pension_pots) }
 
   it { is_expected.to allow_value('joe.bloggs@example.com').for(:email_address) }
   it { is_expected.to_not allow_value('joe @ example.com').for(:email_address) }
@@ -12,4 +13,6 @@ RSpec.describe AppointmentSummary, type: :model do
   it { is_expected.to allow_value('12/02/2015').for(:date_of_appointment) }
   it { is_expected.to_not allow_value('10/02/2012').for(:date_of_appointment) }
   it { is_expected.to_not allow_value(Date.tomorrow.to_s).for(:date_of_appointment) }
+
+  it { is_expected.to allow_value(2000).for(:value_of_pension_pots) }
 end
