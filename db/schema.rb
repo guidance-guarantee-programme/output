6c6e0b216f76d4b1f45802a455a78b4728d7a16c
Andrew Garner <andrew@andrewgarner.com>
2015-03-04 08:57:19 +0000

    Configure MVP authentication
    - Requires a user account to generate a record of guidance.
    - User account creation is through the console by a sysadmin.
    - New accounts must be confirmed
    - Failed login attempts will cause the account to be locked
    - Authenticated sessions will expire after a set period of time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303144601) do
+ActiveRecord::Schema.define(version: 20150304080627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,5 +27,28 @@ ActiveRecord::Schema.define(version: 20150303144601) do
     t.string   "guider_organisation"
     t.string   "guider_name"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                default: "", null: false
+    t.string   "encrypted_password",   default: "", null: false
+    t.integer  "sign_in_count",        default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",      default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
