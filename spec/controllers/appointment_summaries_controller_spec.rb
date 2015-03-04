95bccc2a07284a0bcb7dfbe33d18cac37c3480b3
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-04 11:38:38 +0000

    Test that authentication is required

---
+++ spec/controllers/appointment_summaries_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe AppointmentSummariesController, 'GET #new', type: :controller do
+  let(:email) { 'guider@example.com' }
+  let(:password) { 'pensionwise' }
+  let(:user) do
+    User.create(email: email, password: password).tap(&:confirm!)
+  end
+
+  subject { response }
+
+  context 'when not authenticated' do
+    before do
+      get :new
+    end
+
+    it { is_expected.to redirect_to(controller: 'devise/sessions', action: :new) }
+  end
+
+  context 'when authenticated' do
+    before do
+      sign_in user
+      get :new
+    end
+
+    it { is_expected.to be_ok }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
