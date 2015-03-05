b18ee3b921d8b5886c6ee74c5ff030dce3a34d43
Andrew Garner <andrew@andrewgarner.com>
2015-03-05 14:40:05 +0000

    Set action mailer default URL options

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ Bundler.require(*Rails.groups)
 
 module RecordOfGuidance
   class Application < Rails::Application
+    config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'] }
     config.active_job.queue_adapter = :sidekiq
     config.active_record.raise_in_transactional_callbacks = true
   end
