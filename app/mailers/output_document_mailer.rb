314e419dd899a4acb0144046942e7990ebca6f8e
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-05 12:21:41 +0000

    Add email templates

--- app/mailers/output_document_mailer.rb
+++ app/mailers/output_document_mailer.rb
@@ -1,10 +1,13 @@
 class OutputDocumentMailer < ActionMailer::Base
   def issue(appointment_summary, output_document)
+    @appointment_summary = appointment_summary
+    @subject = 'Record of your guidance'
+
     attachments['record_of_guidance.pdf'] = output_document
     mail(
       to: appointment_summary.email_address,
       from: 'guidance@pensionwise.gov.uk',
-      subject: 'Record of your guidance'
+      subject: @subject
     )
   end
 end
