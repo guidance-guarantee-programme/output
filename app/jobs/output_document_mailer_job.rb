9e14fd74ee3bd3f36debfe006fae6190ce140483
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-18 14:25:59 +0000

    Remove output document mailer job

--- app/jobs/output_document_mailer_job.rb
+++
@@ -1,6 +0,0 @@
-class OutputDocumentMailerJob < ActiveJob::Base
-  def perform(appointment_summary)
-    output_document = OutputDocument.new(appointment_summary)
-    OutputDocumentMailer.issue(appointment_summary, output_document.pdf).deliver_now
-  end
-end
