68d5e7f53c2cc7304f583d5f12412e041e50007d
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-18 17:54:24 +0000

    Progressively enhance form with javascript

---
+++ app/assets/javascripts/pension-pot-accuracy.js
@@ -0,0 +1,29 @@
+(function ($) {
+  'use strict';
+
+  window.PWO = window.PWO || {};
+
+  var pensionPotAccuracy = {
+    init: function () {
+      this.insertHTML();
+      this.cache();
+      this.bindEvents();
+    },
+    insertHTML: function () {
+      var html = $('#pension_pot_accuracy').text();
+      $('#col-pension-pot-value').before(html);
+    },
+    cache: function () {
+      this.$wrapper = $('.display_if_range');
+      this.$input = $('input[name="appointment_summary[accuracy]"]');
+    },
+    bindEvents: function () {
+      this.$input.on('change', function () {
+        $.publish('accuracyChange', this.value);
+      });
+    }
+  };
+
+  PWO.pensionPotAccuracy = pensionPotAccuracy;
+
+})(jQuery);
