9a4977ded61aa9c960ea5e008e694a6667874dde
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-18 17:54:24 +0000

    Progressively enhance form with javascript

---
+++ app/assets/javascripts/consent.js
@@ -0,0 +1,34 @@
+(function ($) {
+  'use strict';
+
+  window.PWO = window.PWO || {};
+
+  var consent = {
+    init: function () {
+      this.insertHTML();
+      this.cache();
+      this.bindEvents();
+    },
+    insertHTML: function () {
+      var html = $('#consent').text();
+      $('.consent').append(html);
+    },
+    cache: function () {
+      this.$wrapper = $('.display_if_consent');
+      this.$input = $('input[name="appointment_summary[consent]"]');
+    },
+    bindEvents: function () {
+      var that = this;
+      this.$input.on('change', function () {
+        if (this.checked) {
+          that.$wrapper.fadeIn();
+        } else {
+          that.$wrapper.fadeOut();
+        }
+      });
+    }
+  };
+
+  PWO.consent = consent;
+
+})(jQuery);
