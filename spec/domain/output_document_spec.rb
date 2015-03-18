44403f9dde3ec850b68bb222527d75c1fe135881
Andrew Garner <andrew@andrewgarner.com>
2015-03-18 15:57:15 +0000

    Collect distinct name fields

--- spec/domain/output_document_spec.rb
+++ spec/domain/output_document_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe OutputDocument do
-  let(:name) { 'Joe Bloggs' }
+  let(:title) { 'Mr' }
+  let(:first_name) { 'Joe' }
+  let(:last_name) { 'Bloggs' }
   let(:output_document) { described_class.new(appointment_summary) }
   let(:appointment_summary) do
     instance_double(AppointmentSummary,
-                    name: name,
+                    title: title,
+                    first_name: first_name,
+                    last_name: last_name,
                     date_of_appointment: Date.today,
                     value_of_pension_pots: 35_000,
                     income_in_retirement: :pension,
@@ -16,6 +20,6 @@ RSpec.describe OutputDocument do
   describe '#html' do
     subject { output_document.html }
 
-    it { is_expected.to include(name) }
+    it { is_expected.to include("#{title} #{first_name} #{last_name}") }
   end
 end
