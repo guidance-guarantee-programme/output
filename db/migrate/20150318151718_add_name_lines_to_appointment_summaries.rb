5cdf5ea4796b8798059ce13c9ad53cc3631673f6
Andrew Garner <andrew@andrewgarner.com>
2015-03-18 15:17:52 +0000

    Add migration to add name fields

---
+++ db/migrate/20150318151718_add_name_lines_to_appointment_summaries.rb
@@ -0,0 +1,9 @@
+class AddNameLinesToAppointmentSummaries < ActiveRecord::Migration
+  def change
+    add_column :appointment_summaries, :title, :string
+    add_column :appointment_summaries, :first_name, :string
+    add_column :appointment_summaries, :last_name, :string
+
+    change_column_null :appointment_summaries, :name, true
+  end
+end
