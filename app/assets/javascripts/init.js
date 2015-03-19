8dda3bfe0150ad16d9dab42b1c0ec662a631091f
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-19 14:06:59 +0000

    Enhance value input

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,7 +2,7 @@ $(function () {
   PWO.consent.init();
   //PWO.eligibility.init();
   PWO.pensionPotAccuracy.init();
-  //PWO.pensionPotInput.init();
+  PWO.pensionPotInput.init();
   //PWO.pensionPotPreview.init();
   PWO.postcodeLookup.init();
 });
