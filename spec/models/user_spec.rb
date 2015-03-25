b97e7b1bb31519d8fd193b128d92e8784f0b7287
Andrew Garner <andrew@andrewgarner.com>
2015-03-25 17:03:57 +0000

    Add first and last name to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,54 @@ RSpec.describe User, type: :model do
   subject { described_class.new }
 
   it { is_expected.to have_many(:appointment_summaries) }
+
+  context 'when a full name is set' do
+    subject { described_class.new(name: 'Joe Bloggs') }
+
+    it 'has a name' do
+      expect(subject.name).to eq('Joe Bloggs')
+    end
+
+    it 'has a first name' do
+      expect(subject.first_name).to eq('Joe')
+    end
+
+    it 'has a last name' do
+      expect(subject.last_name).to eq('Bloggs')
+    end
+  end
+
+  context 'when a first name and last name is set' do
+    subject { described_class.new(first_name: 'Joe', last_name: 'Bloggs') }
+
+    it 'has a name' do
+      expect(subject.name).to eq('Joe Bloggs')
+    end
+
+    it 'has a first name' do
+      expect(subject.first_name).to eq('Joe')
+    end
+
+    it 'has a last name' do
+      expect(subject.last_name).to eq('Bloggs')
+    end
+  end
+
+  context 'with an existing full name' do
+    before do
+      allow(subject).to receive(:read_attribute).with(:name) { 'Joe Bloggs' }
+    end
+
+    it 'has a name' do
+      expect(subject.name).to eq('Joe Bloggs')
+    end
+
+    it 'has a first name' do
+      expect(subject.first_name).to eq('Joe')
+    end
+
+    it 'has a last name' do
+      expect(subject.last_name).to eq('Bloggs')
+    end
+  end
 end
