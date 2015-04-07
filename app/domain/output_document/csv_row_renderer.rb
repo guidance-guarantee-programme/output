8263a33828347802169c99c11a7ed9e32b75904c
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-07 14:20:30 +0100

    Multiple `OutputDocument`s are now rendered with `CSVRenderer`

--- app/domain/output_document/csv_row_renderer.rb
+++ app/domain/output_document/csv_row_renderer.rb
@@ -4,21 +4,13 @@ class OutputDocument
   class CSVRowRenderer
     attr_accessor :output_document
 
-    FIELDS = %i(id format variant attendee_name attendee_address_line_1
-                attendee_address_line_2 attendee_address_line_3 attendee_town
-                attendee_county attendee_postcode lead guider_first_name
-                guider_organisation appointment_reference appointment_date
-                value_of_pension_pots income_in_retirement continue_working
-                unsure leave_inheritance wants_flexibility wants_security
-                wants_lump_sum poor_health)
-
     def initialize(output_document)
       @output_document = output_document
     end
 
     def render
       CSV.generate(col_sep: '|', encoding: 'iso-8859-1') do |csv|
-        csv << FIELDS.map { |field| output_document.public_send(field) }
+        csv << CSVRenderer.headers.map { |field| output_document.public_send(field) }
       end
     end
   end
