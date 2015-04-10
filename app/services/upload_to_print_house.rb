128116798f3cdb29fb151e06c3aa71b6cf4d2bf3
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-10 12:11:33 +0100

    Add method to upload a file via SFTP
    Host, username and password are configured via environment variables

--- app/services/upload_to_print_house.rb
+++ app/services/upload_to_print_house.rb
@@ -1,3 +1,5 @@
+require 'net/sftp'
+
 class UploadToPrintHouse
   attr_accessor :csv
 
@@ -6,5 +8,15 @@ class UploadToPrintHouse
   end
 
   def call
+  end
+
+  private
+
+  def upload_file(path, contents)
+    Net::SFTP.start(ENV['SFTP_HOST'], ENV['SFTP_USER'], password: ENV['SFTP_PASSWORD']) do |sftp|
+      sftp.file.open(path, 'w') do |f|
+        f.puts contents
+      end
+    end
   end
 end
