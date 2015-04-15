4ca0f1a496251001f4e064eda505cdf5d48b5d2e
Gareth Visagie <gareth@gjvis.com>
2015-04-15 17:28:57 +0100

    Move CSV reading from FakeSFTP to CSV matcher

--- features/support/matchers/expect_uploaded_csv_to_include.rb
+++ features/support/matchers/expect_uploaded_csv_to_include.rb
@@ -1,4 +1,22 @@
+require 'csv'
+
+CSV::Converters[:boolean] = lambda do |value|
+  case value.to_s
+  when 'true' then true
+  when 'false' then false
+  else value
+  end
+end
+
 module UploadedCSVMatcher
+  def read_uploaded_csv
+    path = FakeSFTP.find_path('*.csv')
+    contents = FakeSFTP.read(path)
+
+    CSV.parse(contents, headers: true, converters: [:numeric, :date_time, :boolean],
+                        header_converters: :symbol, col_sep: '|')
+  end
+
   def expect_uploaded_csv_to_include(expected)
     rows = read_uploaded_csv
     expect(rows.count).to eq(1), "Expected 1 row, but #{rows.count} rows were found."
