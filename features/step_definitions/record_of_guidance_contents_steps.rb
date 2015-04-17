3f8fce45ae05634db3aa2e483c3de073991a826b
Gareth Visagie <gareth@gjvis.com>
2015-04-17 12:00:50 +0100

    Rename RecordOfGuidancePreviewPage -> PreviewPage

--- features/step_definitions/record_of_guidance_contents_steps.rb
+++ features/step_definitions/record_of_guidance_contents_steps.rb
@@ -28,7 +28,7 @@ When(/^we send (?:them their|a) record of guidance$/) do
   appointment_summary_page.fill_in(@appointment_summary)
   appointment_summary_page.submit.click
 
-  preview_page = RecordOfGuidancePreviewPage.new
+  preview_page = PreviewPage.new
   preview_page.confirm.click
 
   ProcessOutputDocuments.new.call
