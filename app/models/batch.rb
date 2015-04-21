44518b0d5d4e198b14e5092fcef482cde2ddd90b
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-21 15:14:26 +0100

    Destroy join table entries when deleting a `Batch` or `AppointmentSummary`

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,4 @@
 class Batch < ActiveRecord::Base
-  has_many :appointment_summaries_batches
+  has_many :appointment_summaries_batches, dependent: :destroy
   has_many :appointment_summaries, through: :appointment_summaries_batches
 end
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Batch, type: :model do
   it { is_expected.to have_many(:appointment_summaries).through(:appointment_summaries_batches) }
+  it { is_expected.to have_many(:appointment_summaries_batches).dependent(:destroy) }
 end
