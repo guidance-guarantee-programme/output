30348b20f3af458da3ec9a1e042bb8d267f00747
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-23 11:46:02 +0100

    Extract `PostcodeValidator`

--- spec/models/appointment_summary_spec.rb
+++ spec/models/appointment_summary_spec.rb
@@ -51,12 +51,6 @@ RSpec.describe AppointmentSummary, type: :model do
   it { is_expected.to_not validate_presence_of(:county) }
   it { is_expected.to validate_length_of(:county).is_at_most(50) }
   it { is_expected.to validate_presence_of(:postcode) }
-  it { is_expected.to allow_value('sw1a 2hq').for(:postcode) }
-  it { is_expected.to allow_value('SW1A 2HQ').for(:postcode) }
-  it { is_expected.to allow_value('SW1A2HQ').for(:postcode) }
-  it { is_expected.to allow_value(' SW1A 2HQ    ').for(:postcode) }
-  it { is_expected.to_not allow_value('SW1A2H').for(:postcode) }
-  it { is_expected.to_not allow_value('SWIA2HQ').for(:postcode) }
 
   it do
     is_expected
---
+++ spec/validators/postcode_validator_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe PostcodeValidator, type: :model do
+  let(:klass) do
+    Class.new do
+      include ActiveModel::Validations
+
+      attr_accessor :postcode
+
+      validates :postcode, postcode: true
+    end
+  end
+
+  subject { klass.new }
+
+  it { is_expected.to allow_value('sw1a 2hq').for(:postcode) }
+  it { is_expected.to allow_value('SW1A 2HQ').for(:postcode) }
+  it { is_expected.to allow_value('SW1A2HQ').for(:postcode) }
+  it { is_expected.to allow_value(' SW1A 2HQ    ').for(:postcode) }
+  it { is_expected.to_not allow_value('SW1A2H').for(:postcode) }
+  it { is_expected.to_not allow_value('SWIA2HQ').for(:postcode) }
+end
