c559fd33f193d1cbb71f229963a4f158a00590cb
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-27 11:13:30 +0100

    Add Devise recoverable columns to users table

---
+++ db/migrate/20150427100026_add_devise_recoverable_to_users.rb
@@ -0,0 +1,7 @@
+class AddDeviseRecoverableToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+    add_index :users, :reset_password_token, unique: true
+  end
+end
