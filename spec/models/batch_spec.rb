d08ce79f00e28b8727317ed2f8363be93eaebc93
Gareth Visagie <gareth@gjvis.com>
2015-04-27 14:19:10 +0100

    Replace Batch#processed_at with #uploaded_at
    The 'processed_at' column is simply dropped as its not yet used

--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Batch, type: :model do
   it { is_expected.to have_many(:appointment_summaries).through(:appointment_summaries_batches) }
   it { is_expected.to have_many(:appointment_summaries_batches).dependent(:destroy) }
+
+  it 'has a default #uploaded_at of nil' do
+    expect(subject.uploaded_at).to be_nil
+  end
 end
