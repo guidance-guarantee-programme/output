316042ed18671867ae6b99ccad65b76cd4fd23e0
Gareth Visagie <gareth@gjvis.com>
2015-04-27 22:21:02 +0100

    Add notifications and error handling to uploads

--- app/services/print_house_sftp_uploader.rb
+++ app/services/print_house_sftp_uploader.rb
@@ -2,14 +2,21 @@ require 'net/sftp'
 require 'stringio'
 
 class PrintHouseSFTPUploader
+  include Wisper::Publisher
+
   def call(jobs)
-    Array(jobs).each do |job|
-      upload_file(job.payload_path, job.payload)
-      upload_file(job.trigger_path, job.trigger)
-    end
+    Array(jobs).each { |job| process(job) }
   end
 
   private
+
+  def process(job)
+    upload_file(job.payload_path, job.payload)
+    upload_file(job.trigger_path, job.trigger)
+    broadcast(:upload_succeeded, job)
+  rescue => error
+    broadcast(:upload_failed, job, error)
+  end
 
   def upload_file(path, contents)
     io = StringIO.new(contents)
--- spec/services/print_house_sftp_uploader_spec.rb
+++ spec/services/print_house_sftp_uploader_spec.rb
@@ -13,19 +13,66 @@ RSpec.describe PrintHouseSFTPUploader, '#call' do
   let(:uploader) { described_class.new }
   before { allow(uploader).to receive(:upload_file) }
 
-  subject! { uploader.call(jobs) }
+  describe 'file uploads' do
+    subject! { uploader.call(jobs) }
 
-  it 'uploads data from all specified jobs' do
-    jobs.each do |job|
-      expect(uploader).to have_received(:upload_file).with(job.payload_path, job.payload)
-      expect(uploader).to have_received(:upload_file).with(job.trigger_path, job.trigger)
+    it 'uploads data from all specified jobs' do
+      jobs.each do |job|
+        expect(uploader).to have_received(:upload_file).with(job.payload_path, job.payload)
+        expect(uploader).to have_received(:upload_file).with(job.trigger_path, job.trigger)
+      end
+    end
+
+    it 'uploads trigger files after their respective payloads' do
+      jobs.each do |job|
+        expect(uploader).to have_received(:upload_file).with(job.payload_path, job.payload).ordered
+        expect(uploader).to have_received(:upload_file).with(job.trigger_path, job.trigger).ordered
+      end
     end
   end
 
-  it 'uploads trigger files after their respective payloads' do
-    jobs.each do |job|
-      expect(uploader).to have_received(:upload_file).with(job.payload_path, job.payload).ordered
-      expect(uploader).to have_received(:upload_file).with(job.trigger_path, job.trigger).ordered
+  describe 'error handling and notifications' do
+    let(:jobs_that_fail) { [jobs.first] }
+    let(:jobs_that_succeed)  { jobs - jobs_that_fail }
+    let(:failure_notifications) { [] }
+    let(:success_notifications) { [] }
+
+    before do
+      jobs_that_fail.each do |job|
+        allow(uploader).to receive(:upload_file)
+          .with(job.payload_path, job.payload).and_raise('upload failed!')
+      end
+
+      uploader.on(:upload_failed) { |*args| failure_notifications << args }
+      uploader.on(:upload_succeeded) { |*args| success_notifications << args }
+    end
+
+    subject! { uploader.call(jobs) }
+
+    it 'notifies subscribers for each successful upload' do
+      expect(success_notifications.count).to eq(jobs_that_succeed.count)
+    end
+
+    it 'includes the job in success notifications' do
+      successful_jobs = success_notifications.map { |args| args[0] }
+
+      expect(successful_jobs).to eq(jobs_that_succeed)
+    end
+
+    it 'notifies subscribers for each failed upload' do
+      expect(failure_notifications.count).to eq(jobs_that_fail.count)
+    end
+
+    it 'includes the job in failure notifications' do
+      failed_jobs = failure_notifications.map { |args| args[0] }
+
+      expect(failed_jobs).to eq(jobs_that_fail)
+    end
+
+    it 'includes the exception in failure notifications' do
+      exceptions = failure_notifications.map { |args| args[1] }
+
+      expect(exceptions).to all(be_a(StandardError))
     end
   end
 end
