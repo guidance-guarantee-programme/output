e7e171027f04cde4bba3fb1127261a352f527c22
Steven Wilkin <stevenwilkin@gmail.com>
2015-04-29 16:24:03 +0100

    Use tagged logging for key stages of PrintHouse integration

--- app/services/process_output_documents.rb
+++ app/services/process_output_documents.rb
@@ -1,7 +1,20 @@
 class ProcessOutputDocuments
+  attr_reader :tags
+
+  def initialize
+    @tags = [self.class, SecureRandom.uuid]
+  end
+
   def call
+    logger.push_tags(tags)
+    logger.info('Start')
+
     CreateBatch.new.call
     upload_all(Batch.unprocessed)
+
+    logger.info('End')
+  ensure
+    logger.pop_tags(tags.size)
   end
 
   private
@@ -11,8 +24,14 @@ class ProcessOutputDocuments
   end
 
   def upload(batch)
+    logger.info("Processing batch #{batch.id}")
+
     job = CSVUploadJob.new(batch)
     UploadToPrintHouse.new(job).call
     batch.mark_as_uploaded
+  end
+
+  def logger
+    Rails.logger
   end
 end
