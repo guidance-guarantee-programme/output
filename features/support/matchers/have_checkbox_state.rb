e286d3749b3fed94887b6ac6125890679cec3e82
Gareth Visagie <gareth@gjvis.com>
2015-04-30 16:29:24 +0100

    Add checkbox state matcher for back from preview

---
+++ features/support/matchers/have_checkbox_state.rb
@@ -0,0 +1,11 @@
+require 'rspec/expectations'
+
+module CheckedState
+  RSpec::Matchers.define :have_checked_state do |expected_check_state|
+    match do |element|
+      !!element.checked? == !!expected_check_state
+    end
+  end
+end
+
+World(CheckedState)
