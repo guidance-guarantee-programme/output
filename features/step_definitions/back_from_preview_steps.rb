7f26a7c165024072f2ab5ba3ddd49761de79d31b
Gareth Visagie <gareth@gjvis.com>
2015-05-01 16:26:48 +0100

    Refactor populated appointment summary assertions

--- features/step_definitions/back_from_preview_steps.rb
+++ features/step_definitions/back_from_preview_steps.rb
@@ -22,41 +22,46 @@ end
 Then(/^the previously captured details are prepopulated$/) do
   page = AppointmentSummaryPage.new
 
-  expect(page.title.value).to eq(@appointment_summary.title)
-  expect(page.first_name.value).to eq(@appointment_summary.first_name)
-  expect(page.last_name.value).to eq(@appointment_summary.last_name)
+  text_fields = %i(title
+                   first_name
+                   last_name
+                   address_line_1
+                   address_line_2
+                   address_line_3
+                   town
+                   county
+                   postcode
+                   country
+                   reference_number
+                   value_of_pension_pots
+                   upper_value_of_pension_pots
+                   guider_name)
 
-  expect(page.address_line_1.value).to eq(@appointment_summary.address_line_1)
-  expect(page.address_line_2.value).to eq(@appointment_summary.address_line_2)
-  expect(page.address_line_3.value).to eq(@appointment_summary.address_line_3)
-  expect(page.town.value).to eq(@appointment_summary.town)
-  expect(page.county.value).to eq(@appointment_summary.county)
-  expect(page.postcode.value).to eq(@appointment_summary.postcode)
-  expect(page.country.value).to eq(@appointment_summary.country)
+  check_boxes = %i(value_of_pension_pots_is_approximate
+                   continue_working
+                   unsure
+                   leave_inheritance
+                   wants_flexibility
+                   wants_security
+                   wants_lump_sum
+                   poor_health)
+
+  radio_button_groups = %i(income_in_retirement
+                           guider_organisation
+                           has_defined_contribution_pension
+                           format_preference)
+
+  text_fields.each do |field|
+    expect(page.public_send(field).value).to eq(@appointment_summary.public_send(field).to_s)
+  end
+
+  check_boxes.each do |field|
+    expect(page.public_send(field)).to have_checked_state(@appointment_summary.public_send(field))
+  end
+
+  radio_button_groups.each do |field|
+    expect(page.public_send(field)).to have_selected_value(@appointment_summary.public_send(field))
+  end
 
   expect(Time.zone.parse(page.date_of_appointment.value)).to eq(@appointment_summary.date_of_appointment)
-  expect(page.reference_number.value).to eq(@appointment_summary.reference_number)
-
-  expect(page.value_of_pension_pots.value).to eq(@appointment_summary.value_of_pension_pots.to_s)
-  expect(page.upper_value_of_pension_pots.value).to eq(@appointment_summary.upper_value_of_pension_pots.to_s)
-  expect(page.value_of_pension_pots_is_approximate)
-    .to have_checked_state(@appointment_summary.value_of_pension_pots_is_approximate?)
-
-  expect(page.income_in_retirement).to have_selected_value(@appointment_summary.income_in_retirement)
-
-  expect(page.guider_name.value).to eq(@appointment_summary.guider_name)
-  expect(page.guider_organisation).to have_selected_value(@appointment_summary.guider_organisation)
-
-  expect(page.has_defined_contribution_pension)
-    .to have_selected_value(@appointment_summary.has_defined_contribution_pension)
-
-  expect(page.continue_working).to have_checked_state(@appointment_summary.continue_working?)
-  expect(page.unsure).to have_checked_state(@appointment_summary.unsure?)
-  expect(page.leave_inheritance).to have_checked_state(@appointment_summary.leave_inheritance?)
-  expect(page.wants_flexibility).to have_checked_state(@appointment_summary.wants_flexibility?)
-  expect(page.wants_security).to have_checked_state(@appointment_summary.wants_security?)
-  expect(page.wants_lump_sum).to have_checked_state(@appointment_summary.wants_lump_sum?)
-  expect(page.poor_health).to have_checked_state(@appointment_summary.poor_health?)
-
-  expect(page.format_preference).to have_selected_value(@appointment_summary.format_preference)
 end
