142a0356a1453e8c845b7471df23e17c06865962
Steven Wilkin <stevenwilkin@gmail.com>
2015-05-06 12:21:17 +0100

    Don't pad appointment date with blanks

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,3 @@
 # https://www.gov.uk/service-manual/user-centred-design/resources/patterns/dates.html#formatting-dateso
 
-Date::DATE_FORMATS[:gov_uk] = '%e %B %Y'
+Date::DATE_FORMATS[:gov_uk] = '%-d %B %Y'
