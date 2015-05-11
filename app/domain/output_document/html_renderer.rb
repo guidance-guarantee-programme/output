9b663423d577ce9eee6dba562abb7d84a4800829
Gareth Visagie <gareth@gjvis.com>
2015-05-11 18:07:18 +0100

    Use output-templates to render html previews

--- app/domain/output_document/html_renderer.rb
+++ app/domain/output_document/html_renderer.rb
@@ -2,62 +2,22 @@ class OutputDocument
   class HTMLRenderer
     include ActionView::Helpers::TextHelper
 
-    attr_accessor :output_document, :template_dir
-    private :output_document=, :template_dir=
+    attr_accessor :output_document
 
-    def initialize(output_document, template_dir = Rails.root.join('app', 'templates'))
+    def initialize(output_document)
       @output_document = output_document
-      @template_dir = template_dir
-    end
-
-    def pages_to_render
-      case output_document.variant
-      when 'tailored'
-        [:covering_letter, :introduction, pension_pot_version, :options_overview,
-         applicable_circumstances, :other_information].flatten
-      when 'generic'
-        [:covering_letter, :introduction, pension_pot_version, :options_overview,
-         :generic_guidance, :other_information]
-      when 'other'
-        [:ineligible]
-      end
-    end
-
-    def stylesheet(filename)
-      css = Sass.compile(ERB.new(
-        File.read(Rails.root.join('app', 'assets', 'stylesheets', filename))
-      ).result(binding))
-      "<style>\n#{css}\n</style>".html_safe
     end
 
     def render
-      ERB.new(template).result(binding)
+      template.render(output_document)
     end
 
     private
 
-    def applicable_circumstances
-      %i(continue_working unsure leave_inheritance
-         wants_flexibility wants_security wants_lump_sum
-         poor_health).select do |c|
-        output_document.public_send(c)
-      end
-    end
-
-    def pension_pot_version
-      return :pension_pot_pension if output_document.income_in_retirement.blank? # sensible default
-
-      :"pension_pot_#{output_document.income_in_retirement}"
-    end
-
     def template
-      [:header, pages_to_render, :footer].flatten.reduce('') do |result, section|
-        result << template_for(section) << "\n\n"
-      end
-    end
+      template_id = (output_document.variant == 'other') ? 'ineligible' : output_document.variant
 
-    def template_for(section)
-      File.read(File.join(template_dir, "output_document_#{section}.html.erb"))
+      Output::Templates.template(template_id)
     end
   end
 end
