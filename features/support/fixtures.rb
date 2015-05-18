e3072c047f403cf50a8d5ef5cf90fb68a6b7bcca
Andrew Garner <andrew@andrewgarner.com>
2015-05-18 10:05:15 +0100

    Approximate non-ASCII characters
    The font we use to render output documents has a
    limited character set so we need to remove
    unsupported characters from appointment summaries.

--- features/support/fixtures.rb
+++ features/support/fixtures.rb
@@ -7,11 +7,11 @@ module Fixtures
     populated_appointment_summary: proc do
       AppointmentSummary.new(
         title: 'Mr',
-        first_name: 'Joe',
-        last_name: 'Bloggs',
+        first_name: 'Joé',
+        last_name: 'Bløggs',
         address_line_1: 'HM Treasury',
-        address_line_2: '1 Horse Guards Road',
-        address_line_3: 'Westminster',
+        address_line_2: '1 Hôrse Guârds Roåd',
+        address_line_3: 'Weštminstęr',
         town: 'London',
         county: 'Greater London',
         country: 'United Kingdom',
@@ -22,7 +22,7 @@ module Fixtures
         upper_value_of_pension_pots: 55_000,
         value_of_pension_pots_is_approximate: true,
         income_in_retirement: 'pension',
-        guider_name: 'Penelope',
+        guider_name: 'Pênelopę',
         guider_organisation: 'tpas',
         has_defined_contribution_pension: 'yes',
         continue_working: true,
@@ -34,6 +34,34 @@ module Fixtures
         poor_health: true,
         format_preference: 'standard'
       )
+    end,
+
+    populated_csv: proc do
+      {
+        format: 'standard',
+        variant: 'tailored',
+        guider_first_name: 'Penelope',
+        guider_organisation: 'The Pensions Advisory Service',
+        appointment_date: '5 February 2015',
+        lead: 'You recently had a Pension Wise guidance appointment with Penelope from ' \
+              'The Pensions Advisory Service on 5 February 2015.',
+        value_of_pension_pots: '£35,000 to £55,000',
+        income_in_retirement: 'pension',
+        attendee_name: 'Mr Joe Bloggs',
+        attendee_address_line_1: 'HM Treasury',
+        attendee_address_line_2: '1 Horse Guards Road',
+        attendee_address_line_3: 'Westminster',
+        attendee_town: 'London',
+        attendee_county: 'Greater London',
+        attendee_postcode: 'SW1A 2HQ',
+        continue_working: true,
+        leave_inheritance: true,
+        poor_health: true,
+        unsure: true,
+        wants_flexibility: true,
+        wants_lump_sum: true,
+        wants_security: true
+      }
     end
   }
 
