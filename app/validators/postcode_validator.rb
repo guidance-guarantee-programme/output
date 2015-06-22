bcd66a0a03e741c231bf44340909a836ba2cd1de
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-22 17:25:59 +0100

    Use uk_postcode to validate postcode format
    NOTE: mistakes with I/1 and O/0 are corrected with this lib, see
          https://github.com/threedaymonk/uk_postcode

--- app/validators/postcode_validator.rb
+++ app/validators/postcode_validator.rb
@@ -2,8 +2,7 @@ class PostcodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    result = Faraday.get("https://api.postcodes.io/postcodes/#{URI.escape(value)}/validate")
-    valid_postcode = result.success? && JSON.parse(result.body)['result']
+    valid_postcode = UKPostcode.parse(value).full_valid?
 
     record.errors.add(attribute, 'is not a postcode') unless valid_postcode
   end
