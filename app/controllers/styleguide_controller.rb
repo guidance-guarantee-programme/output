eb6be3e1d36efe892a1f4bdb5b5f9ae230e887be
Steven Wilkin <stevenwilkin@gmail.com>
2015-12-01 10:34:51 +0000

    Fix styleguide

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -5,4 +5,8 @@ class StyleguideController < ApplicationController
   def pages_input
     render template: 'styleguide/pages/input'
   end
+
+  def pages_output_elements
+    render template: 'styleguide/pages/output_elements'
+  end
 end
