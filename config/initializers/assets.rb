4d32686a8b06375fefd7b16af92324b043013611
Ben Barnett <ben@brushfiredesign.net>
2016-02-25 15:16:07 +0000

    Adds GovUK favicon for development environment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
-Rails.application.config.assets.precompile += %w( govuk_admin_template/favicon.png )
+Rails.application.config.assets.precompile += %w( govuk_admin_template/favicon.png
+                                                  govuk_admin_template/favicon-development.png )
 Rails.application.config.assets.version = '1.0'
