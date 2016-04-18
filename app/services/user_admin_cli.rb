5672c0bcaae2d929e7848a70263ddf7e01b75934
Ben Lovell <benjamin.lovell@gmail.com>
2016-04-18 11:47:21 +0100

    Rake task for toggling `admin` on `User`
    Toggles the `admin` flag for the given `User`. Prints the list of
    configured users after.

--- app/services/user_admin_cli.rb
+++ app/services/user_admin_cli.rb
@@ -3,4 +3,12 @@ class UserAdminCli
   def admins
     User.admins.map { |u| "#{u.name} <#{u.email}>" }
   end
+
+  def toggle(email)
+    return unless email.present?
+
+    User.find_by(email: email).tap do |u|
+      u&.toggle!(:admin)
+    end
+  end
 end
--- spec/services/user_admin_cli_spec.rb
+++ spec/services/user_admin_cli_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe UserAdminCli do
       expect(subject.admins).to match_array(['Rick Sanchez <rick@example.com>'])
     end
   end
+
+  describe '#toggle' do
+    context 'for an existing user' do
+      it 'toggles the `admin` flag' do
+        expect(subject.toggle('rick@example.com')).not_to be_admin
+        expect(subject.toggle('rick@example.com')).to be_admin
+      end
+    end
+
+    context 'for a non-existent user' do
+      it 'returns nil' do
+        expect(subject.toggle('whoops@example.com')).to be_falsey
+      end
+    end
+  end
 end
