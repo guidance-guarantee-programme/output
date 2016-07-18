2b1595626d76b1ae534626c628273137e499444e
David Henry <dw_henry@yahoo.com.au>
2016-07-18 15:37:58 +0100

    Fix positioning of numeric error messages
    If a field is tagged as `js-number-only` and a
    non-numerical value is input, the error class will
    appear on the screen and then fade out.
    
    Changed the code so the error text appears next to
    the input box that generated the error with support
    for multiple error locations on a single form.

--- app/assets/javascripts/pension-pot-input.js
+++ app/assets/javascripts/pension-pot-input.js
@@ -53,7 +53,8 @@
         //if the letter is not digit then display error and don't type anything
         if (e.which !== 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
           //display error message
-          $('#errmsg').html('Only use numbers').show().fadeOut('slow');
+          var error_input = $(e.target).parents('.form-group').find('.js-error-message');
+          error_input.html('Only use numbers').show().attr('aria-live', 'polite').fadeOut('slow');
           return false;
         }
       });
