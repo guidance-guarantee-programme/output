e313acae2f20c328d08531d12f4bcef7446723e0
David Henry <dw_henry@yahoo.com.au>
2016-07-20 16:49:33 +0100

    Reset Pension Pot Accuracy inline with Summary Generator
    if no defined pension pot is set to no and then back to yes:
    * accuracy to deselected 
    * count of pension pots is cleared

--- app/assets/javascripts/pension-pot-input.js
+++ app/assets/javascripts/pension-pot-input.js
@@ -19,6 +19,8 @@
       this.$inputs = $('.js-numbers-only');
       this.$input1 = $('#appointment_summary_value_of_pension_pots');
       this.$input2 = $('#appointment_summary_upper_value_of_pension_pots');
+      this.$potCountInput = $('.js-pot-count');
+      this.$accuracyInput = $('input[name="appointment_summary[accuracy]"]');
     },
     subscribe: function () {
       var that = this;
@@ -36,6 +38,7 @@
           that.$input1.trigger('keyup');
           that.$input2.val('');
           that.$input2.trigger('keyup');
+          that.$potCountInput.val('').trigger('keyup');
         }
       });
 
@@ -45,6 +48,8 @@
           that.$input1.trigger('keyup');
           that.$input2.val('');
           that.$input2.trigger('keyup');
+          that.$potCountInput.val('').trigger('keyup');
+          that.$accuracyInput.prop('checked', false);
         }
       });
     },
