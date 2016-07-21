caa897d5021e3b12873df08b1f06b896713e02f2
David Henry <dw_henry@yahoo.com.au>
2016-07-21 14:30:26 +0100

    Add email field to summary generator
    This adds an email field which is required if a
    digital copy is requested.

--- app/controllers/appointment_summaries_controller.rb
+++ app/controllers/appointment_summaries_controller.rb
@@ -40,20 +40,6 @@ class AppointmentSummariesController < ApplicationController
   private
 
   def appointment_summary_params
-    params.require(:appointment_summary).permit(:title, :first_name, :last_name,
-                                                :address_line_1, :address_line_2, :address_line_3,
-                                                :town, :county, :postcode, :country, :date_of_appointment,
-                                                :reference_number, :value_of_pension_pots,
-                                                :upper_value_of_pension_pots, :value_of_pension_pots_is_approximate,
-                                                :income_in_retirement, :guider_name,
-                                                :guider_organisation, :continue_working, :unsure,
-                                                :leave_inheritance, :wants_flexibility,
-                                                :wants_security, :wants_lump_sum, :poor_health,
-                                                :has_defined_contribution_pension,
-                                                :format_preference, :supplementary_benefits,
-                                                :supplementary_debt, :supplementary_ill_health,
-                                                :supplementary_defined_benefit_pensions,
-                                                :appointment_type, :requested_digital,
-                                                :number_of_previous_appointments, :count_of_pension_pots)
+    params.require(:appointment_summary).permit(AppointmentSummary.editable_column_names)
   end
 end
