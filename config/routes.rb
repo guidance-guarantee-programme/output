b03f74ec28800b79ba91052200d812e2db9e9353
David Henry <david@decoybecoy.com>
2017-01-24 09:40:42 +0000

    Authenticate for sidekiq panel
    This ensures only authenticated users are permitted access
    to the sidekiq control panel.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 require 'sidekiq/api'
 require 'sidekiq/web'
 
-if ENV['SIDEKIQ_USERNAME'] && ENV['SIDEKIQ_PASSWORD']
-  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
-  end
-end
-
 Rails.application.routes.draw do
   root 'appointment_summaries#new'
 
   mount GovukAdminTemplate::Engine, at: '/style-guide'
+  mount Sidekiq::Web => '/sidekiq', constraints: AuthenticatedUser.new
 
   resources :appointment_summaries, only: %i(index new create show) do
     collection do
@@ -31,6 +26,4 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :appointment_summaries, only: [:index, :edit, :update]
   end
-
-  mount Sidekiq::Web => '/sidekiq'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,15 +5,16 @@ require_relative '../config/environment'
 require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
+Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include UserHelpers
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
 end
-
-require_relative 'support/fix_shoulda_bug'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
