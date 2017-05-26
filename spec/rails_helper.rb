c91e2f9974cf8f1c27cbba82c93d1b551c61bdfc
Jon Gilbraith <jon@hobocode.io>
2017-05-26 13:58:34 +0100

    Move Site::Prism pages into `spec` directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,6 @@ require_relative '../config/environment'
 require 'rspec/rails'
 require 'database_rewinder'
 
-require_relative '../features/support/pages/done_page'
-require_relative '../features/support/pages/confirmation_page'
-require_relative '../features/support/pages/appointment_summaries_page'
-require_relative '../features/support/pages/appointment_summary_page'
-require_relative '../features/support/pages/appointment_summary_browser_page'
-require_relative '../features/support/pages/appointment_summary_edit_page'
-
 ActiveRecord::Migration.maintain_test_schema!
 Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
 
