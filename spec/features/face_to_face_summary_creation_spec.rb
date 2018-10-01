148ed56281cb30327a6408d3acc784aa25bd41df
Ben Lovell <benjamin.lovell@gmail.com>
2018-10-01 14:43:25 +0100

    Correct issue with disabled/readonly field
    When the field is marked with the `disabled` attribute it does not POST
    a value, thus would be preselected as `standard` for the
    `appointment_type`. This means some customers were missing out on vital
    information pertaining to the pensionable age.
    
    I also scoped the test helper methods as they were clobbering eachother
    in the global namespace.

--- spec/features/face_to_face_summary_creation_spec.rb
+++ spec/features/face_to_face_summary_creation_spec.rb
@@ -20,40 +20,40 @@ RSpec.feature 'Face to face appointment summary creation' do
     and_the_customer_should_be_notified_by_email
     and_no_activity_should_be_created_on_tap
   end
-end
 
-def given_i_am_logged_in_as_a_face_to_face_guider
-  create(:user, :face_to_face_guider)
-end
+  def given_i_am_logged_in_as_a_face_to_face_guider
+    create(:user, :face_to_face_guider)
+  end
 
-def given_i_am_logged_in_as_a_general_guider
-  create(:user, :general_guider)
-end
+  def given_i_am_logged_in_as_a_general_guider
+    create(:user, :general_guider)
+  end
 
-def when_i_load_the_blank_summary_form
-  @appointment_summary_page = AppointmentSummaryPage.new
-  @appointment_summary_page.load
-end
+  def when_i_load_the_blank_summary_form
+    @appointment_summary_page = AppointmentSummaryPage.new
+    @appointment_summary_page.load
+  end
 
-def and_i_complete_the_summary_form_with_preset_digital_delivery_data
-  template = build(:populated_appointment_summary, :requested_digital)
-  @appointment_summary_page.fill_in(template, face_to_face: true)
-end
+  def and_i_complete_the_summary_form_with_preset_digital_delivery_data
+    template = build(:populated_appointment_summary, :requested_digital)
+    @appointment_summary_page.fill_in(template, face_to_face: true)
+  end
 
-def then_i_am_able_to_submit_and_confirm_successfully
-  @appointment_summary_page.submit.click
+  def then_i_am_able_to_submit_and_confirm_successfully
+    @appointment_summary_page.submit.click
 
-  @confirmation_page = ConfirmationPage.new
-  @confirmation_page.confirm.click
+    @confirmation_page = ConfirmationPage.new
+    @confirmation_page.confirm.click
 
-  @done_page = DonePage.new
-  expect(@done_page).to be_displayed
-end
+    @done_page = DonePage.new
+    expect(@done_page).to be_displayed
+  end
 
-def and_the_customer_should_be_notified_by_email
-  assert_enqueued_jobs 1, only: NotifyViaEmail
-end
+  def and_the_customer_should_be_notified_by_email
+    assert_enqueued_jobs 1, only: NotifyViaEmail
+  end
 
-def and_no_activity_should_be_created_on_tap
-  assert_enqueued_jobs 0, only: CreateTapActivity
+  def and_no_activity_should_be_created_on_tap
+    assert_enqueued_jobs 0, only: CreateTapActivity
+  end
 end
