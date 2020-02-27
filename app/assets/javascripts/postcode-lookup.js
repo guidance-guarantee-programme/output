616a280f4eaf7d8eaa48d45994d9af4af201d41c
Ben Lovell <benjamin.lovell@gmail.com>
2020-02-27 09:55:09 +0000

    WIP postcode search using Google API

--- app/assets/javascripts/postcode-lookup.js
+++ app/assets/javascripts/postcode-lookup.js
@@ -1,5 +1,3 @@
-//= require jquery.postcodes/dist/postcodes.min.js
-
 (function ($) {
   'use strict';
 
@@ -7,30 +5,54 @@
 
   var postcodeLookup = {
     init: function () {
-      this.insertHTML();
-      this.initPostcodeLookup();
+      this.postcode = document.getElementById('autocomplete-postcode');
+      this.clear = $('#postcode-reset');
+      this.autocomplete = new google.maps.places.Autocomplete(this.postcode, { types: ['geocode'], componentRestrictions: {country: 'uk'} });
+
+      this.initAutoComplete();
+      this.clear.on('click', this.clearAddress.bind(this));
     },
-    insertHTML: function () {
-      var html = $('#postcode-lookup-template').html();
-      $('#postal-address-heading').after(html);
+
+    initAutoComplete: function () {
+      this.autocomplete.setFields(['address_components']);
+      google.maps.event.addListener(this.autocomplete, 'place_changed', this.onPlaceChanged.bind(this));
     },
-    initPostcodeLookup: function () {
-      $('#postcode-lookup').setupPostcodeLookup({
-        address_search: 20,
-        api_key: 'ak_i7dldo8cLN8H59JMUiERpXDPWuFR6',
-        input: '#postcode-lookup-input',
-        button: '#postcode-lookup-button',
-        dropdown_class: 'form-control input-md-3',
-        dropdown_container: '#postcode-lookup-results-container',
-        output_fields: {
-          line_1: '#appointment_summary_address_line_1',
-          line_2: '#appointment_summary_address_line_2',
-          line_3: '#appointment_summary_address_line_3',
-          post_town: '#appointment_summary_town',
-          postcode: '#appointment_summary_postcode'
+
+    clearAddress: function() {
+      var addressFields = [
+        '#appointment_summary_address_line_1',
+        '#appointment_summary_address_line_2',
+        '#appointment_summary_address_line_3',
+        '#appointment_summary_town',
+        '#appointment_summary_county',
+        '#appointment_summary_postcode'
+      ];
+
+      this.postcode.value = '';
+
+      for(var i = 0; i < addressFields.length; i++) {
+        $(addressFields[i]).val('');
+      }
+    },
+
+    onPlaceChanged: function() {
+      var place = this.autocomplete.getPlace();
+      var components = { postal_town: '#appointment_summary_town', postal_code: '#appointment_summary_postcode' };
+
+      if(place.address_components) {
+        place = place.address_components;
+
+        $('#appointment_summary_address_line_1').val(place[0].long_name + ' ' + place[1].long_name);
+
+        for (var i = 2; i < place.length; i++) {
+          var addressType = place[i].types[0];
+
+          if (components[addressType]) {
+            $(components[addressType]).val(place[i].long_name);
+          }
         }
-      });
-    }
+      }
+    },
   };
 
   PWO.postcodeLookup = postcodeLookup;
