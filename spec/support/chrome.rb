c3162248c27e2be79129b2e3d6bde86b3a53cff8
Ben Lovell <benjamin.lovell@gmail.com>
2023-08-11 10:58:13 +0100

    Pin chromedriver

--- spec/support/chrome.rb
+++ spec/support/chrome.rb
@@ -7,6 +7,9 @@ Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
+Webdrivers::Chromedriver.required_version = '114.0.5735.90'
+Webdrivers.install_dir = '/opt/homebrew/bin' unless ENV['TRAVIS']
+
 Capybara.default_max_wait_time = 10 if ENV['TRAVIS']
 Capybara.server = :puma, { Silent: true }
 Capybara.javascript_driver = :chrome
